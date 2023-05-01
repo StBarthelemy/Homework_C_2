89a57e2f0ee1601fe19d9543d8e702c14bbf6543
StBarthelemy <130822796+StBarthelemy@users.noreply.github.com>
2023-05-01 09:01:11 +0300

    Готово

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 100;
+int b = number % 10;
+int c = (number - a*100 - b)/10;
+Console.WriteLine($"{number}");
+Console.WriteLine($"{c}");
